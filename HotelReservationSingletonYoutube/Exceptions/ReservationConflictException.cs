2cb6af2896833f57083da5386d48e7fe13713ecb
TrinhPC2020 <trinhquanhn1992@gmail.com>
2023-12-31 15:16:26 +0700

    add views folder

--- HotelReservationSingletonYoutube/Exceptions/ReservationConflictException.cs
+++ HotelReservationSingletonYoutube/Exceptions/ReservationConflictException.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HotelReservationSingletonYoutube.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,26 @@ using System.Threading.Tasks;
 
 namespace HotelReservationSingletonYoutube.Exceptions
 {
-    internal class ReservationConflictException
+    public class ReservationConflictException : Exception
     {
+        public Reservation ExistingReservation { get; }
+        public Reservation IncomingReservation { get; }
+
+        public ReservationConflictException(Reservation existingReservation, Reservation incomingReservation)
+        {
+            ExistingReservation = existingReservation;
+            IncomingReservation = incomingReservation;
+        }
+        public ReservationConflictException(string message, Reservation existingReservation, Reservation incomingReservation) : base(message)
+        {
+            ExistingReservation = existingReservation;
+            IncomingReservation = incomingReservation;
+        }
+
+        public ReservationConflictException(string message, Exception innerException, Reservation existingReservation, Reservation incomingReservation) : base(message, innerException)
+        {
+            ExistingReservation = existingReservation;
+            IncomingReservation = incomingReservation;
+        }
     }
 }
